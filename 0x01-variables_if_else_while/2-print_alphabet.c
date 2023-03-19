2134acbecd6a866281ac7243c416e0bea215c0f3
mounirkaali <mounirkaali@gmail.com>
2023-03-19 15:53:14 -0700

    display the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - 'display the alphabet'
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 122; c++)
+	{
+		putchar(c);
+	}
+	printf("\n");
+	return (0);
+}
+
