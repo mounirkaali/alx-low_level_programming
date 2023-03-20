0a56981cea28d94a321ef05164980557ec553817
mounirkaali <mounirkaali@gmail.com>
2023-03-20 15:49:16 -0700

    print numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - 'display the numbers in base 16 using putchar'
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
