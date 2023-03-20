de008ee76ba592f95f6c3050db17cc46517311bd
mounirkaali <mounirkaali@gmail.com>
2023-03-20 15:31:22 -0700

    print numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - 'display the numbers until 10'
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
