fe55c560fefea91bfd658a68a997103871aa8e8b
mounirkaali <mounirkaali@gmail.com>
2023-03-22 15:18:11 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point function
+ * _putchar : print the alphabets putchar
+ * Return : main return always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
